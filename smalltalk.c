e35c822bbd0d82cf60e2bd2a8c55920282a9d1b1
Mathieu Suen <mathieu.suen@yahoo.fr>
2010-12-14 18:34:34 +0100

    Add unary tags + start keyword tags

--- smalltalk.c
+++ smalltalk.c
@@ -13,6 +13,7 @@
 #include "general.h"	/* must always come first */
 
 #include <string.h>
+#include <stdbool.h>
 
 #include "parse.h"      /* always include */
 #include "read.h"       /* to define file fileReadLine() */
@@ -30,12 +31,25 @@ static void
 installSmalltalk(const langType);
 
 /*
-  Callback Method call when a class definition is founded
+  Callback function for class definition
  */
 static void
 smalltalkClass (const char *const, const regexMatch *const,
 				const unsigned int);
 
+/*
+  Callback function for keyword method definition
+ */
+static void
+smalltalkKeywordMethod (const char *const, const regexMatch *const,
+				const unsigned int);
+
+/*
+  Callback method for unary method declaration
+ */
+static void
+smalltalkUnaryMethod (const char *const, const regexMatch *const,
+				const unsigned int);
 
 
 /* DATA DEFINITIONS */
@@ -47,7 +61,7 @@ typedef enum eSmalltalkKinds {
 static kindOption SmalltalkKinds [] = {
   {TRUE, 'c', "class", "Classes"},
   {TRUE, 'm', "method", "Object's methods"}
-}
+};
 
 
 /* Create parser definition stucture */
@@ -56,8 +70,8 @@ SmalltalkParser (void)
 {
     static const char *const extensions [] = { "st", NULL };
     parserDefinition* def = parserNew ("Smalltalk");
-    def->kinds      = smalltalkKinds;
-    def->kindCount  = KIND_COUNT (smalltalkKinds);
+    def->kinds      = SmalltalkKinds;
+    def->kindCount  = KIND_COUNT (SmalltalkKinds);
     def->extensions = extensions;
     def->parser     = findSmalltalkTags;
 	def->initialize = installSmalltalk;
@@ -67,12 +81,14 @@ SmalltalkParser (void)
 static void
 installSmalltalk (const langType language)
 {
-  addCallbackRegex (language, "^[A-Z][A-Za-z0-9_]+[ \t]subclass:[ \t]([A-Z][A-Za-z0-9_]+)[[ \t]", NULL, smalltalkClass);
+  addCallbackRegex (language, "^[ \t]*[A-Z][A-Za-z0-9_]+[ \t]subclass:[ \t]([A-Z][A-Za-z0-9_]+)[[ \t]", NULL, smalltalkClass);
+  addCallbackRegex (language, "^[ \t]*([A-Za-z_][A-Za-z0-9_]*)[ \t]*[[]", NULL, smalltalkUnaryMethod);
+  addCallbackRegex (language, "^[ \t]*([A-Za-z_][A-Za-z0-9_]*:[ \t]?[A-Za-z_][A-Za-z0-9_]*[ \t]?)+[[]", NULL, smalltalkKeywordMethod);
 }
 
 static void
 smalltalkClass (const char *const line, const regexMatch *const matches,
-				const unsigned int cound)
+				const unsigned int count)
 {
   if (count > 1) /* should always be true per regex */
 	{
@@ -83,8 +99,55 @@ smalltalkClass (const char *const line, const regexMatch *const matches,
 }
 
 static void
+smalltalkUnaryMethod (const char *const line, const regexMatch *const matches,
+				const unsigned int count)
+{
+  if (count > 1) /* should always be true per regex */
+	{
+	  vString *const name = vStringNew ();
+	  vStringNCopyS (name, line + matches [1].start, matches [1].length);
+	  makeSimpleTag (name, SmalltalkKinds, K_METHOD);
+	}
+}
+
+static vString 
+nextToken(vString line)
+{
+  
+}
+
+
+static void
+smalltalkKeywordMethod (const char *const line, const regexMatch * const matches,
+				const unsigned int count)
+{
+  bool copy = true;
+  if (count > 2) /* should always be true per regex */
+	{
+	  vString *const matchString = vStringNew ();
+	  vString *const name = vStringNew ();
+	  vStringNCopyS (matchString, line + matches [0].start, matches [0].length);
+	  vStringStripLeading (matchString);
+	  vStringStripTrailing (matchString);
+	  for(int i = 0; i < matchString.length; i++)
+		{
+		  if (copy)
+			{
+			  vStringPut (name, vStringChar (matchString, i));
+			}
+		  if (vStringItem (matchString, i) == ':')
+			{
+			  
+			}
+		}
+	  makeSimpleTag (name, SmalltalkKinds, K_METHOD);
+	}
+}
+
+static void
 findSmalltalkTags (void)
 {
   while (fileReadLine () != NULL)
 	; /* All is done in a callback */
+}
 
