2577201567a66a3faa310d5c25e9743e6995c436
Mathieu Suen <mathieu.suen@yahoo.fr>
2010-12-16 14:53:45 +0100

    Fix typo

--- smalltalk.c
+++ smalltalk.c
@@ -4,7 +4,7 @@
 *   This source code is released for free distribution under the terms of the
 *   GNU General Public License.
 *
-*   This module contains functions for generating tags for Objective Caml
+*   This module contains functions for generating tags for Smalltalk
 *   language files.
 */
 /*
